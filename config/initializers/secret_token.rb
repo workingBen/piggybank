214bca7fce9070880aa61649c4b6fcd38ce64692
Ben Pellow <benjamin.pellow@gmail.com>
2013-02-13 17:56:20 -0800

    rails new piggybank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piggybank::Application.config.secret_token = '2b5af41b72e69b84eb7ad5a5d053346fd695ab051c3f9a8ed2aa77442f7c26dcf83ab33f4bad0c9109239d7daf2a40d12d06f8342f035205d2e264e18c8a5680'
