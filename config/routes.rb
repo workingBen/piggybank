b20ad214e60edaaaf633651738882a43fd61e75c
Ben Pellow <benjamin.pellow@gmail.com>
2013-02-27 14:42:10 -0800

    faq route and view

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Piggybank::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  match 'faq' => 'static#faq'
   root :to => 'static#welcome'
 
   # See how all your routes lay out with "rake routes"
