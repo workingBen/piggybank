eb885bf0270bb0b0d00a2c3332c74487935a990a
Ben Pellow <benjamin.pellow@gmail.com>
2013-03-09 15:10:14 -0800

    User.type methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  attr_accessible :type
+
   # types
   GRANTOR      = 0
   BENEFICIARY  = 1
   CONTRIBUTOR  = 2
 
-  # attr_accessible :title, :body
+  validates_presence_of :type
+
+  def self.by_grantor;       where(type: GRANTOR) end
+  def self.by_beneficiary; where(type: BENEFICIARY) end
+  def self.by_contributor;  where(type: CONTRIBUTOR) end
+  def self.by_investor;  where("type in %w(#{GRANTOR}, #{CONTRIBUTOR})") end
+  def grantor?;       type == User::GRANTOR end
+  def beneficiary?; type == User::BENEFICIARY end
+  def contributor?;  type == User::CONTRIBUTOR end
+  def investor?;  (type == User::CONTRIBUTOR || type == User::GRANTOR) end
+
 end
