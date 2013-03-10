502aae5e5955ca22b9060af1c6d6618a8848de77
Ben Pellow <benjamin.pellow@gmail.com>
2013-03-09 16:04:22 -0800

    init devise

--- db/migrate/20130309223909_create_users.rb
+++ db/migrate/20130309223909_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.integer :user_type
-      t.string :email
       t.string :first_name
       t.string :last_name
       t.string :street
